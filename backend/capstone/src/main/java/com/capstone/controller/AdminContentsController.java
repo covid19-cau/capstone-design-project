8746e84707c58ec1e56c11a27dbff1e3f9a5b29b
seungkyu2 <zx6658@naver.com>
2020-10-11 17:31:49 +0900

    feat: deal with cors error

--- backend/capstone/src/main/java/com/capstone/controller/AdminContentsController.java
+++ backend/capstone/src/main/java/com/capstone/controller/AdminContentsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.capstone.dao.EquipmentDao;
 import com.capstone.dao.HomeTrainingDao;
@@ -22,97 +23,96 @@ import com.capstone.model.HomeTraining;
 import com.capstone.model.Meal;
 import com.capstone.model.User;
 
+@CrossOrigin()
 @RestController
 public class AdminContentsController {
-	
-	@Autowired
-	private EquipmentDao equipmentDao;
-	
-	@Autowired
-	private MealDao mealDao;
-	
-	@Autowired
-	private HomeTrainingDao hometrainingDao;
-	
-	@GetMapping("/admin/hometraining")
-	public List<HomeTraining> listHomeTraining(){
-		return hometrainingDao.findAllHomeTraining();
-	}
-	
-	@GetMapping("/admin/meal")
-	public List<Meal> listMeal(){
-		return mealDao.findAllMeal();
-	}
-	
-	@GetMapping("/admin/equipment")
-	public List<Equipment> listEquipment(){
-		return equipmentDao.findAllEquipment();
-	}
 
-	@PostMapping("/admin/hometraining/add")
-	public ResponseEntity<HomeTraining> addHomeTrainingData(@RequestBody HomeTraining ht) {
-		hometrainingDao.saveHomeTraining(ht);
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(ht.getTitle()).toUri();
-		return ResponseEntity.created(location).build();
-	}
-	
-	@PostMapping("/admin/meal/add")
-	public ResponseEntity<Meal> addMealData(@RequestBody Meal meal) {
-		mealDao.saveMeal(meal);
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(meal.getId()).toUri();
-		return ResponseEntity.created(location).build();
-	}
-	
-	@PostMapping("/admin/equipment/add")
-	public ResponseEntity<Equipment> addEquipmentData(@RequestBody Equipment equipment) {
-		equipmentDao.saveEquipment(equipment);
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(equipment.getId()).toUri();
-		return ResponseEntity.created(location).build();
-	}
-	
-	@DeleteMapping("admin/hometraining/delete/{id}")
-	public void deleteHomeTrainingData(@RequestParam String id) {
-		hometrainingDao.deleteHomeTraining(id);
-	}
-	
-	@DeleteMapping("admin/meal/delete/{id}")
-	public void deleteMealData(@RequestParam String id) {
-		mealDao.deleteMeal(id);
-	}
-	
-	@DeleteMapping("admin/equipment/delete/{id}")
-	public void deleteequipmentData(@RequestParam String id) {
-		equipmentDao.deleteEquipment(id);
-	}
-	
-	@PutMapping("admin/hometraining/modify/{id}")
-	public void modifyHomeTrainingData(@RequestParam String id, @RequestBody HomeTraining ht) {
-		HomeTraining one = hometrainingDao.findByID(id);
-		if(one == null) {
-			throw new ContentsNotFoundException(String.format("ID[%s] HomeTraining info not found", id));
-		}
-		hometrainingDao.replaceHomeTraining(id,ht);
-	}
-	
-	@PutMapping("admin/meal/modify/{id}")
-	public void modifyMealData(@RequestParam String id, @RequestBody Meal meal) {
-		Meal one = mealDao.findByID(id);
-		if(one == null) {
-			throw new ContentsNotFoundException(String.format("ID[%s] meal info not found", id));
-		}
-		mealDao.replaceMeal(id,meal);
-	}
-	
-	@PutMapping("admin/equipment/modify/{id}")
-	public void modifyEquipmentData(@RequestParam String id, @RequestBody Equipment equipment) {
-		Equipment one = equipmentDao.findByID(id);
-		if(one == null) {
-			throw new ContentsNotFoundException(String.format("ID[%s] equipment info not found", id));
-		}
-		equipmentDao.replaceEquipment(id,equipment);
-	}
-	
+  @Autowired
+  private EquipmentDao equipmentDao;
+
+  @Autowired
+  private MealDao mealDao;
+
+  @Autowired
+  private HomeTrainingDao hometrainingDao;
+
+  @GetMapping("/admin/hometraining")
+  public List<HomeTraining> listHomeTraining() {
+    return hometrainingDao.findAllHomeTraining();
+  }
+
+  @GetMapping("/admin/meal")
+  public List<Meal> listMeal() {
+    return mealDao.findAllMeal();
+  }
+
+  @GetMapping("/admin/equipment")
+  public List<Equipment> listEquipment() {
+    return equipmentDao.findAllEquipment();
+  }
+
+  @PostMapping("/admin/hometraining/add")
+  public ResponseEntity<HomeTraining> addHomeTrainingData(@RequestBody HomeTraining ht) {
+    hometrainingDao.saveHomeTraining(ht);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(ht.getTitle()).toUri();
+    return ResponseEntity.created(location).build();
+  }
+
+  @PostMapping("/admin/meal/add")
+  public ResponseEntity<Meal> addMealData(@RequestBody Meal meal) {
+    mealDao.saveMeal(meal);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(meal.getId()).toUri();
+    return ResponseEntity.created(location).build();
+  }
+
+  @PostMapping("/admin/equipment/add")
+  public ResponseEntity<Equipment> addEquipmentData(@RequestBody Equipment equipment) {
+    equipmentDao.saveEquipment(equipment);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(equipment.getId())
+        .toUri();
+    return ResponseEntity.created(location).build();
+  }
+
+  @DeleteMapping("admin/hometraining/delete/{id}")
+  public void deleteHomeTrainingData(@RequestParam String id) {
+    hometrainingDao.deleteHomeTraining(id);
+  }
+
+  @DeleteMapping("admin/meal/delete/{id}")
+  public void deleteMealData(@RequestParam String id) {
+    mealDao.deleteMeal(id);
+  }
+
+  @DeleteMapping("admin/equipment/delete/{id}")
+  public void deleteequipmentData(@RequestParam String id) {
+    equipmentDao.deleteEquipment(id);
+  }
+
+  @PutMapping("admin/hometraining/modify/{id}")
+  public void modifyHomeTrainingData(@RequestParam String id, @RequestBody HomeTraining ht) {
+    HomeTraining one = hometrainingDao.findByID(id);
+    if (one == null) {
+      throw new ContentsNotFoundException(String.format("ID[%s] HomeTraining info not found", id));
+    }
+    hometrainingDao.replaceHomeTraining(id, ht);
+  }
+
+  @PutMapping("admin/meal/modify/{id}")
+  public void modifyMealData(@RequestParam String id, @RequestBody Meal meal) {
+    Meal one = mealDao.findByID(id);
+    if (one == null) {
+      throw new ContentsNotFoundException(String.format("ID[%s] meal info not found", id));
+    }
+    mealDao.replaceMeal(id, meal);
+  }
+
+  @PutMapping("admin/equipment/modify/{id}")
+  public void modifyEquipmentData(@RequestParam String id, @RequestBody Equipment equipment) {
+    Equipment one = equipmentDao.findByID(id);
+    if (one == null) {
+      throw new ContentsNotFoundException(String.format("ID[%s] equipment info not found", id));
+    }
+    equipmentDao.replaceEquipment(id, equipment);
+  }
+
 }
