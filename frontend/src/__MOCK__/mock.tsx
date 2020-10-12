5576b4a3cfd9bb46b844f5c930edebc6521b01a5
seungkyu2 <zx6658@naver.com>
2020-10-12 19:27:43 +0900

    feat: map api response to frontend

--- frontend/src/__MOCK__/mock.tsx
+++ frontend/src/__MOCK__/mock.tsx
@@ -3,6 +3,122 @@ import { Tag, Badge, Space } from "antd";
 
 export const recommendTags = ["muscle", "diet", "body-shape"];
 
+export enum dataColumn {
+  video = "video",
+  meal = "meal",
+  equipment = "equipment",
+  user = "user",
+}
+
+export const dataColumns = {
+  [dataColumn.video]: [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+      render: (text: any) => <p>{text}</p>,
+    },
+    {
+      title: "Url",
+      dataIndex: "url",
+      key: "url",
+      render: (text: any) => <a>{text}</a>,
+    },
+    {
+      title: "Uploader",
+      dataIndex: "uploader",
+      key: "uploader",
+      render: (text: any) => <Tag>{text}</Tag>,
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text: any, record: any) => (
+        <Space size="middle">
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ],
+  [dataColumn.meal]: [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text: any) => <p>{text}</p>,
+    },
+    {
+      title: "Carbohydrate",
+      dataIndex: "carbohydrate",
+      key: "carbohydrate",
+      render: (text: any) => <Tag color="geekblue">{text}</Tag>,
+    },
+    {
+      title: "protein",
+      dataIndex: "protein",
+      key: "protein",
+      render: (text: any) => <Tag color="green">{text}</Tag>,
+    },
+    {
+      title: "fat",
+      dataIndex: "fat",
+      key: "fat",
+      render: (text: any) => <Tag color="red">{text}</Tag>,
+    },
+    {
+      title: "calory",
+      dataIndex: "calory",
+      key: "calory",
+      render: (text: any) => <Tag>{text}</Tag>,
+    },
+    {
+      title: "serving_size",
+      dataIndex: "serving_size",
+      key: "serving_size",
+
+      render: (text: any) => <p>{text}</p>,
+    },
+    {
+      title: "training_purpose",
+      dataIndex: "training_purpose",
+      key: "training_purpose",
+
+      render: (text: any) => <p>{text}</p>,
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text: any, record: any) => (
+        <Space size="middle">
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ],
+  [dataColumn.equipment]: [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+      render: (text: any) => <a>{text}</a>,
+    },
+    {
+      title: "Content",
+      dataIndex: "content",
+      key: "content",
+    },
+
+    {
+      title: "Action",
+      key: "action",
+      render: (text: any, record: any) => (
+        <Space size="middle">
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ],
+};
 export const columns = [
   {
     title: "Title",
@@ -15,26 +131,7 @@ export const columns = [
     dataIndex: "content",
     key: "content",
   },
-  {
-    title: "Subject",
-    key: "subject",
-    dataIndex: "subject",
-    render: (tags: any) => (
-      <>
-        {tags.map((tag: any) => {
-          let color = tag.length > 5 ? "geekblue" : "green";
-          if (tag === "loser") {
-            color = "volcano";
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
+
   {
     title: "Action",
     key: "action",
