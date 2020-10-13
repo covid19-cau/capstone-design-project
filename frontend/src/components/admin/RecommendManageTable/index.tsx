2b18408ce2ae8d2c2f15bbe07fedac2ca1b86427
seungkyu2 <zx6658@naver.com>
2020-10-13 22:21:28 +0900

    feat: add register contents features

--- frontend/src/components/admin/RecommendManageTable/index.tsx
+++ frontend/src/components/admin/RecommendManageTable/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Table, Button, Input, Select } from "antd";
+import { Table, Button, Input, Select, Form } from "antd";
 import { ColumnsType } from "antd/lib/table";
 
 import { dataColumn, recommendTags } from "__MOCK__/mock";
@@ -14,17 +14,38 @@ interface IProps {
   columns: ColumnsType<any>;
   data: any;
   selectedKey: dataColumn;
+  registerContents: (params: object) => void;
 }
+
+const recommendKeys = {
+  [dataColumn.meal]: [
+    "name",
+    "serving_size",
+    "calory",
+    "carbohydrate",
+    "fat",
+    "protein",
+  ],
+  [dataColumn.video]: ["name", "title", "uploader", "URL"],
+  [dataColumn.equipment]: ["name", "price", "shipping_charge", "seller", "URL"],
+  [dataColumn.user]: ["name", "serving_size", "calory", "carbohydrate", "fat"],
+};
 
 const RecommendManageTable: React.FC<IProps> = ({
   columns,
   data,
   selectedKey,
+  registerContents,
 }) => {
   const [visible, setVisible] = useState(false);
   const onChange = (value: any) => {
     console.log(`selected ${value}`);
   };
+  const onFinish = (values: any) => {
+    registerContents(values);
+    setVisible(false);
+  };
+  const [form] = Form.useForm();
 
   return (
     <div>
@@ -40,32 +61,39 @@ const RecommendManageTable: React.FC<IProps> = ({
         setVisible={setVisible}
         title="Recommend Contents"
       >
-        <div className={styles.wrapper}>
-          <h5>Title</h5>
-          <Input placeholder="title" />
-        </div>
-        <div className={styles.wrapper}>
-          <h5>Content</h5>
-          <Input placeholder="content" />
-        </div>
-        {selectedKey === dataColumn.video && (
+        <Form name="basic" form={form} onFinish={onFinish}>
+          {recommendKeys[selectedKey].map((key: any) => {
+            return (
+              <div className={styles.wrapper}>
+                <Form.Item
+                  label={key}
+                  name={key}
+                  rules={[{ required: true, message: `Please input ${key}!` }]}
+                >
+                  <Input placeholder={key} />
+                </Form.Item>
+              </div>
+            );
+          })}
+
           <div className={styles.wrapper}>
-            <h5>Url</h5>
-            <Input placeholder="url" />
+            <h5>Tags</h5>
+            <Form.Item label="Training purpose" name="training_purpose">
+              <Select
+                style={{ width: 200 }}
+                placeholder="Training purpose"
+                onChange={onChange}
+              >
+                {recommendTags.map((tag) => {
+                  return <Option value={tag}>{tag}</Option>;
+                })}
+              </Select>
+            </Form.Item>
           </div>
-        )}
-        <div className={styles.wrapper}>
-          <h5>Tags</h5>
-          <Select
-            style={{ width: 200 }}
-            placeholder="Select a tag"
-            onChange={onChange}
-          >
-            {recommendTags.map((tag) => {
-              return <Option value={tag}>{tag}</Option>;
-            })}
-          </Select>
-        </div>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form>
       </ContentsModal>
       <Table columns={columns} dataSource={data} />
     </div>
